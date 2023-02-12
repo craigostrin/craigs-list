e53ac2d8a6ae3547d8a6e1e947cbf5e3ba642d56
craig <craigostrin@gmail.com>
2023-02-11 23:16:32 -0800

    A lot

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 class Item {
-    constructor(name, notes = ""){
+    constructor(name, notes = "", image = ""){
+        this.Id = getId();
         this.name = name;
         this.notes = notes;
+        this.image = image;
     }
 }
 
 class Furniture extends Item {
-    constructor(name, dims="", notes=""){
-        super(name, notes);
+    constructor(name, dims="", notes="", image = ""){
+        super(name, notes, image);
         this.dims = dims;
     }
+}
+
+const items = {
+    furniture: {},
+    games: {},
+    electronics: {},
+    other: {},
+};
+
+let _nextId = 0;
+
+
+function getId() {
+    _nextId++;
+    return _nextId;
 }
