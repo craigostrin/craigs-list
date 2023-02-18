45629524f0b1393ffd4723506d0fce6a12623a0f
craig <craigostrin@gmail.com>
2023-02-18 14:47:08 -0800

    Add email functionality

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
-class Item {
-    constructor(name, notes = "", image = ""){
-        this.Id = getId();
-        this.name = name;
-        this.notes = notes;
-        this.image = image;
-    }
+const inputName    = document.querySelector('#input-name');
+const inputContact = document.querySelector('#input-contact');
+const submitButton = document.querySelector('#submit-button');
+
+
+submitButton.addEventListener('click', () => {
+    let from    = inputName.value;
+    let contact = inputContact.value;
+    let items   = getInterestedItemsAsString();
+    console.log(from + " is interested in " + items + ", contact info: " + contact);
+    sendEmail(from, contact, items);
+});
+
+
+function getInterestedItemsAsString(){
+    const checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+    console.log(checkboxes);
+    const interestedChecked = checkboxes.filter( checkbox => checkbox.checked );
+    console.log(interestedChecked);
+    let interestedItems = interestedChecked.map( checkbox => {
+        return checkbox.parentNode.nextSibling.nextSibling.textContent;
+    });
+    return interestedItems.join(", ");
 }
 
-class Furniture extends Item {
-    constructor(name, dims="", notes="", image = ""){
-        super(name, notes, image);
-        this.dims = dims;
-    }
-}
 
-const items = {
-    furniture: {},
-    games: {},
-    electronics: {},
-    other: {},
-};
-
-let _nextId = 0;
-
-
-function getId() {
-    _nextId++;
-    return _nextId;
+function sendEmail(from, contact, items){
+    Email.send({
+        SecureToken : 'aff6275e-420b-4980-bad4-e20ff2c206ea',
+        To : 'craigostrin@gmail.com',
+        From : 'craigostrin@gmail.com',
+        Subject : `craigs-list alert from ${from}`,
+        Body : `${from} is interested in: ${items}....Contact info: ${contact}`
+    }).then(
+      message => {
+        if (message === "OK") {
+            alert("Submitted. Thanks for your interested! I'll be in touch. :)")
+        } else {
+            alert(message);
+        }
+      }
+    );
 }
